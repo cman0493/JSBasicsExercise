7b79301a0dfe8afc0ff4ea9c958b71d56cf43c50
Chris Hunley <cshunley21@gmail.com>
2021-09-20 14:28:26 -0500

    Completed first version of exercise

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("21");
+// Numbers, Strings, Booleans, Null, Undefined
+// +, -, /, *, %
+console.log(5%4);
+console.log(5%2);
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+console.log("Hawk" + "eye");
+console.log("Christopher Hunley".length);
+console.log("Avengers: Endgame"[16]);
+console.log("/");
+console.log("I-Am-Iron/Man".length =0);
